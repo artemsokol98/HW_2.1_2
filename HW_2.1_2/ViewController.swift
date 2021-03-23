9433cc16fdaa0808f051a2cfc60441e60107f27a
Artem Sokolovskiy <artemsokol98@gmail.com>
2021-03-23 12:38:06 +0300

    Initial Commit

---
+++ HW_2.1_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_2.1_2
+//
+//  Created by Артем Соколовский on 23.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
